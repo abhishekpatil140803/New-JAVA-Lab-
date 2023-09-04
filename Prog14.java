21b5f953fa4143eceb2b4a8b67e80e59535fae1f
abhishekpatil140803 <143088683+abhishekpatil140803@users.noreply.github.com>
2023-09-04 22:32:08 +0530

    Add files via upload

---
+++ Prog14.java
@@ -0,0 +1,10 @@
+//14.	Demonstrate using Instance/class  Variable in a Java Program by creating a simple public class
+
+public class Prog14{
+    int x=5;
+    public static void main(String[] args) {
+        Prog14 obj=new Prog14();
+        System.out.print("The value assigned to instance variable is "+obj.x);
+    }
+}
+
