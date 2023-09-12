1385a858381b7233d3b9e6a28fdd4ffc414b2704
abhishekpatil140803 <143088683+abhishekpatil140803@users.noreply.github.com>
2023-09-12 21:45:14 +0530

    Add files via upload

--- egg.java
+++ egg.java
@@ -1,22 +1,27 @@
 class Name{
     String name;
     String surname;
-    Name(String name,String surname)
+    Name(String name,String surname) 
     {
         this.name=name;
         this.surname=surname;
     }
+   
+    
+    
     public static Name function(Name n1,Name n2)
     {
         return new Name(n1.name+n1.surname+n2.surname,n2.name+n2.surname);
     }
 }
-public class egg{
+
+class egg{
     public static void main(String args[])
     {
-        Name n1=new Name("Aishwarya","Rai");
-        Name n2=new Name("Abhishek","Bachan");
+        Name n1=new Name("Aishwariya","Rai");
+        Name n2=new Name("Abhishek","Bacchan");
         Name n3=Name.function(n1,n2);
-        System.out.println(n3.name+" "+n3.surname+" ");
+        System.out.println(n3.name+"+"+n3.surname+" ");
     }
+   
 }
