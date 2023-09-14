cb82593df112be9e985c3d95b4111acf6ace0684
abhishekpatil140803 <143088683+abhishekpatil140803@users.noreply.github.com>
2023-09-14 11:50:47 +0530

    Add files via upload

---
+++ String Java.java
@@ -0,0 +1,15 @@
+
+//String in Java 
+class Main
+{
+    //create string 
+    public static void main (String[]args){
+    String first ="Java";
+    String second ="python";
+    String third ="JavaScript";
+    //print String
+    System.out.println("first");
+    System.out.println("second");
+    System.out.println("third");
+    }
+}
